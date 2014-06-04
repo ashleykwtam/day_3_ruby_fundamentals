6cff00db58dc6ee947b2e85bbaa24a4fa44cd18f
Ashley <ashleykwtam@gmail.com>
2014-06-04 14:36:30 -0400

    changed stylistic conventions and created expansion method

--- exercise7.rb
+++ exercise7.rb
@@ -8,6 +8,16 @@ students = {
 
 students[:cohort4] = 43
 
-students.each {|key, value| puts "In #{key} there are #{value} students."}
+students.each { |key, value| puts "In #{key} there are #{value} students."; }
 
-students.each {|key, value| puts "The cohorts are: #{key}"}
+puts "\n"  		#adds extra blank line
+
+students.each { |key, value| puts "The cohorts are: #{key}" }
+
+puts "\n"
+
+students.each do |k, v| 
+	v = (v * 1.05).round(0)
+	puts "The expansion in each #{k} is #{v}"
+	students[k] = v 			 #sets v as new value permanently
+end
