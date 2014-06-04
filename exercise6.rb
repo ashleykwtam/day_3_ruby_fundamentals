a342eb2256ca9e4f5742e76bfd67cf12c9f08edb
Ashley <ashleykwtam@gmail.com>
2014-06-04 13:27:56 -0400

    added .length and reminder to pick up bananas

--- exercise6.rb
+++ exercise6.rb
@@ -6,3 +6,12 @@ grocery_list.each do |item|
 	puts "* "+item 					#need " " to separate * and item
 end
 
+puts "There are #{grocery_list.length} items on my list."
+
+grocery_list.include? ("bananas")
+if grocery_list.include? ("bananas")
+	puts "You need to pick up bananas."
+else
+	puts "No need to pick up bananas."
+end
+
