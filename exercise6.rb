4dc8e6e0a9b8c0e90cb5aa0b38fd912b9f223e2f
Ashley <ashleykwtam@gmail.com>
2014-06-04 13:18:48 -0400

    added extra item to grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -1,6 +1,6 @@
 # Shopping list exercise
 
-grocery_list = ["carrots", "toilet paper", "spinach", "apples", "almonds"]
+grocery_list = ["carrots", "toilet paper", "spinach", "apples", "almonds", "rice"]
 
 grocery_list.each do |item|
 	puts "* "+item 					#need " " to separate * and item
