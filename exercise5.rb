572b15ac995b7572142e603c7bd047a025810eb8
Ashley <ashleykwtam@gmail.com>
2014-06-04 12:44:29 -0400

    added ending sentence and rounding to three digits

--- exercise5.rb
+++ exercise5.rb
@@ -2,7 +2,7 @@ puts "What is the temperature in Fahrenheit?"
 fahrenheit = gets.chomp.to_i
 
 def convert(fahrenheit) #can only be lower case
-	return (fahrenheit-32)*(5.0/9)
+	return ((fahrenheit-32)*(5.0/9)).round(3)
 end
 
-
+puts "The temperature in Celcius is #{convert(fahrenheit)}."
