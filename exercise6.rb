940eb36879bd0e1bbb8cd21f51c290f862c77c19
Ashley <ashleykwtam@gmail.com>
2014-06-04 13:17:47 -0400

    present grocery list with an item on each line, including an asterisk

---
+++ exercise6.rb
@@ -0,0 +1,8 @@
+# Shopping list exercise
+
+grocery_list = ["carrots", "toilet paper", "spinach", "apples", "almonds"]
+
+grocery_list.each do |item|
+	puts "* "+item 					#need " " to separate * and item
+end
+
